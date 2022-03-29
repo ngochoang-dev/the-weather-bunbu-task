a2e203ab1b5fff418c4adf8bfb8d706d98af5e43
ngochoang.dev <55541339+ngochoang-monster@users.noreply.github.com>
2022-03-29 16:02:58 +0700

    update

--- client/src/Components/ModalCreate/ModalCreate.js
+++ client/src/Components/ModalCreate/ModalCreate.js
@@ -32,9 +32,22 @@ function ModalCreate({
             setShowModal(false)
     }, [loading, setShowModal]);
 
+    function removeAscent(str) {
+        if (str === null || str === undefined) return str;
+        str = str.toLowerCase();
+        str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+        str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+        str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+        str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+        str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+        str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+        str = str.replace(/đ/g, "d");
+        return str;
+    }
+
     const handleInputChange = (e) => {
         const regex = /^[a-zA-Z0-9 ]*$/;
-        (!regex.test(e.target.value) || e.target.value.length === 20) ?
+        (!regex.test(removeAscent(e.target.value)) || e.target.value.length === 20) ?
             setIsValid(true) :
             setIsValid(false)
         setCityName(e.target.value)
